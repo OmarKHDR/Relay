843e36d76da93db0278ae4e06e60353bdd7065a2
Ahmed Araby <ahmed.araby7956@gmail.com>
2025-10-05 12:55:11 +0300

    add cors

--- src/index.js
+++ src/index.js
@@ -5,26 +5,28 @@ import timestamper from '#utils/timestamp.js';
 import apiRouter from '#routes';
 import express from 'express';
 import morgan from 'morgan';
+import cors from 'cors';
 
 // connections info
-const serverHostname= process.env.SERVER_HOSTNAME;
-const serverPort= process.env.SERVER_PORT;
-
+const serverHostname = process.env.SERVER_HOSTNAME;
+const serverPort = process.env.SERVER_PORT;
 
 const app = express();
 
 //create a logger for logging the requests
-app.use(morgan(":method :url :status :response-time ms",{
-	stream: {
-		write: (message) => logger.info(message.trim()),
-		},
-	})
+app.use(
+    morgan(':method :url :status :response-time ms', {
+        stream: {
+            write: message => logger.info(message.trim()),
+        },
+    })
 );
 
 app.use(express.json());
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use('/api/v1', apiRouter);
 
 app.listen(serverPort, serverHostname, () => {
-	logger.info(`server is running on ${serverHostname}:${serverPort}`);
+    logger.info(`server is running on ${serverHostname}:${serverPort}`);
 });
