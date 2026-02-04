a1b2f65a70d623e391f026cc67c53acf686c3aa2
OmarKHDR <omarsliman37@gmail.com>
2026-02-04 10:25:08 +0200

    new way of initializing the server, more control, less implementation details

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import dotenv from 'dotenv';
 import logger from '#utils/logger.js';
-import websocketServer, {httpServer} from '#servers/websocketServer.js';
+import Server from '#servers/server.js';
+import { swaggerOptions } from '../swagger.options.js';
+import { ApiRegistry } from "#servers/apiRegistry.js"
+import { CallbackRegistry } from '#servers/callbackRegistry.js';
 
 dotenv.config();
 
@@ -8,11 +11,29 @@ const PORT = process.env.SERVER_PORT || 3000;
 const HOST = process.env.SERVER_HOSTNAME || 'localhost';
 
 
+const server = new Server();
+
+server.configureExpress({
+        "logging": logger,
+        "docs": {
+            swaggerOptions,
+            prefix: "/api/v1",
+            endpoint: "/docs"
+        },
+})
+
+server.configureWS({
+    "CORS": "*"
+})
+
+server.registerModules({
+    ApiRegistry,
+    CallbackRegistry,
+})
+
+server.start({
+    "host": HOST,
+    "port": PORT
+})
 
 
-// Start the server
-httpServer.listen(PORT, HOST, () => {
-    logger.info(`HTTP & WS running on http://${HOST}:${PORT}`);
-    logger.info(`WebSocket controllers registered: ${websocketServer.callbackRegistry.length}`);
-    logger.info(`Active connections: ${websocketServer.getConnectionCount()}`);
-});
--- src/servers/websocketServer.js
+++ src/servers/websocketServer.js
@@ -1,107 +1,101 @@
 import { Server } from 'socket.io';
 import logger from '#utils/logger.js';
-import { CallbackRegistry } from '#servers/callbackRegistry.js';
-import expressApp from '#servers/expressServer.js';
-import http from 'http';
 
-
-
-
-class WebsocketServer {
-    constructor(options = {}) {
-        if (WebsocketServer.instance) {
-            return WebsocketServer.instance;
-        }
-        this.io = null;
-        this.httpServer = http.createServer(expressApp);
-        this.callbackRegistry = CallbackRegistry;
-        this.initialize(options);
-        WebsocketServer.instance = this;
+export default class WebsocketServer {
+constructor(httpServer) {
+    if (WebsocketServer.instance) {
+        return WebsocketServer.instance;
     }
-
-    initialize(options = {}) {
-        if (this.io) {
-            logger.warn('WebSocket server already initialized');
-            return this;
-        }
-
-        this.io = new Server(this.httpServer, {
-            cors: {
-                origin: options.cors || '*',
-                methods: ['GET', 'POST']
-            },
-            ...options
-        });
-
-        // Handle connections
-        this.registerBroadCallback();
-        this.io.on('connection', socket => {
-            logger.info(`Socket Connected: ${socket.id}`);
-
-            this.registerPubCallback(socket);
-            this.registerSubCallback(socket);
-
-            socket.on('disconnect', reason => {
-                logger.info(`Socket Disconnected: ${socket.id}, Reason: ${reason}`);
-                this.cleanSockets(socket);
-            });
-
-            socket.on('error', error => {
-                logger.error(`Socket Error: ${socket.id}`, error);
-            });
-        });
-
-        logger.info('WebSocket server initialized');
-        return this;
-    }
-
-    //if anyone publish on an event the callback is triggered
-    registerPubCallback(socket) {
-        this.callbackRegistry.forEach(registry => {
-            registry.publish.forEach(pub => {
-                socket.on(pub.eventName, data => {
-                    pub.callback(data)
-                })
-            })
-        })
-    }
-
-
-
-    registerSubCallback(socket) {
-        this.callbackRegistry.forEach(registry => {
-            registry.subscribe.forEach(sub => {
-                sub.callback(socket, sub.eventName)
-            })
-        })
-    }
-
-    registerBroadCallback() {
-        this.callbackRegistry.forEach(registry => {
-            registry.broadcast.forEach(broad => {
-                broad.callback(this.io, broad.eventName)
-            })
-        })
-    }
-
-    cleanSockets(socket) {
-        this.callbackRegistry.forEach(registry => {
-            registry.cleanup(socket)
-        })
-    }
-    getIO() {
-        if (!this.io) {
-            throw new Error('WebSocket server not initialized. Call initialize() first.');
-        }
-        return this.io;
-    }
-
-    // Get number of connected clients
-    getConnectionCount() {
-        return this.io?.sockets.sockets.size || 0;
-    }
+    this.io = null;
+    this.httpServer = httpServer;
+    WebsocketServer.instance = this;
 }
 
-const websocketServer = new WebsocketServer();
-export const httpServer = websocketServer.httpServer;
-export default websocketServer;
+reset() {
+    WebsocketServer.instance = null;
+}
+
+createWS(options = {}) {
+    this.io = new Server(this.httpServer, {
+        cors: {
+            origin: options.cors || '*',
+            methods: ['GET', 'POST'],
+        },
+        ...options,
+    });
+    return this
+}
+
+registerCallback(callbackRegistry) {
+    if (this.io.initialized) {
+        logger.warn('WebSocket server already initialized');
+        return this;
+    }
+    //register broadcast once with the IO instance
+    this.registerBroadCallback(callbackRegistry);
+    this.io.on('connection', socket => {
+        logger.info(`Socket Connected: ${socket.id}`);
+        
+        this.registerPubCallback(callbackRegistry, socket);
+        this.registerSubCallback(callbackRegistry, socket);
+
+        socket.on('disconnect', reason => {
+            logger.info(`Socket Disconnected: ${socket.id}, Reason: ${reason}`);
+            this.cleanSockets(callbackRegistry, socket);
+        });
+        
+        socket.on('error', error => {
+            logger.error(`Socket Error: ${socket.id}`, error);
+        });
+    });
+    this.io.initialized = true;
+
+    logger.info('WebSocket server initialized');
+    return this;
+}
+
+//the user published data
+registerPubCallback(callbackRegistry, socket) {
+    callbackRegistry.forEach(registry => {
+        registry.publish.forEach(pub => {
+            socket.on(pub.eventName, data => {
+                pub.callback(data);
+            });
+        });
+    });
+}
+
+// the user (socket) is subscribing to an event name
+registerSubCallback(callbackRegistry, socket) {
+    callbackRegistry.forEach(registry => {
+        registry.subscribe.forEach(sub => {
+            sub.callback(socket, sub.eventName);
+        });
+    });
+}
+
+registerBroadCallback(callbackRegistry) {
+    callbackRegistry.forEach(registry => {
+        registry.broadcast.forEach(broad => {
+            broad.callback(this.io, broad.eventName);
+        });
+    });
+}
+
+cleanSockets(callbackRegistry, socket) {
+    callbackRegistry.forEach(registry => {
+        registry.cleanup(socket);
+    });
+}
+getIO() {
+    if (!this.io) {
+        throw new Error('WebSocket server not initialized. Call initialize() first.');
+    }
+    return this.io;
+}
+
+// Get number of connected clients
+getConnectionCount() {
+    return this.io?.sockets.sockets.size || 0;
+}
+}
