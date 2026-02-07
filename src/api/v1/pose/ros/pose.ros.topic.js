6b062b20cdedb6eb15a28179afdd964ed9038cb8
OmarKHDR <omarsliman37@gmail.com>
2026-02-07 20:21:00 +0200

    [UPDATE POSE ROS TOPIC]:\n | instead of using the ACML topic we now use directly the transformation from the map frame to the base_link

--- src/api/v1/pose/ros/pose.ros.topic.js
+++ src/api/v1/pose/ros/pose.ros.topic.js
@@ -7,39 +7,49 @@ class Pose extends EventEmitter {
     constructor() {
         if (Pose.instance) return Pose.instance;
         super();
-
         this.ros = rosHandler.getRos();
         this.pose = null;
-
-        this.topic = new ROSLIB.Topic({
+        
+        // Create TF client to get transforms
+        this.tfClient = new ROSLIB.TFClient({
             ros: this.ros,
-            name: '/amcl_pose',
-            messageType: 'geometry_msgs/PoseWithCovarianceStamped',
+            fixedFrame: 'map',           // Global reference frame
+            angularThres: 0.01,          // Angular threshold for updates (radians)
+            transThres: 0.01,            // Translation threshold for updates (meters)
+            rate: 10.0,                  // Update rate in Hz
         });
-
+        
         this.subscribe();
         Pose.instance = this;
     }
 
     subscribe() {
-        this.topic.subscribe(msg => {
-            const p = msg.pose.pose;
-            const yaw = quaternionToYaw(p.orientation);
-
+        // Subscribe to transform from 'map' to 'base_link'
+        // Note: Use 'base_footprint' if your robot uses that instead
+        this.tfClient.subscribe('base_link', (tf) => {
+            const yaw = quaternionToYaw(tf.rotation);
             const newPose = {
-                frame: msg.header.frame_id, // "map"
-                x: p.position.x,
-                y: p.position.y,
+                frame: 'map',
+                x: tf.translation.x,
+                y: tf.translation.y,
                 yaw,
-				stamp: msg.header.stamp?.secs * 1000 || Date.now(),
+                stamp: Date.now(),
             };
-
+            
             this.pose = newPose;
+            this.emit('pose', newPose); // Emit event for listeners
         });
     }
 
     getPose() {
         return this.pose;
+    }
+    
+    // Cleanup method for graceful shutdown
+    unsubscribe() {
+        if (this.tfClient) {
+            this.tfClient.unsubscribe('base_link');
+        }
     }
 }
 
