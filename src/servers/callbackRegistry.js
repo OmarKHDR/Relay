764d03bdeacb29abb6a40fc7e3c260abf276c7af
OmarKHDR <omarsliman37@gmail.com>
2026-02-09 23:38:00 +0200

    removing unused modules

--- src/servers/callbackRegistry.js
+++ src/servers/callbackRegistry.js
@@ -1,14 +1,10 @@
 import { MapModule } from '#modules/map/map.module.js';
-import { ServoModule } from '#modules/servo/servo.module.js';
-import { UltrasonicModule } from '#modules/ultrasonic/ultrasonic.module.js';
 import { WheelchairModule } from '#modules/wheelchair/wheelchair.module.js';
 import { PoseModule } from '#modules/pose/pose.module.js';
 import { GoalModule } from '#modules/goal/goal.module.js';
 
 export const CallbackRegistry = [
     ...MapModule.WSCallback,
-    ...ServoModule.WSCallback,
-    ...UltrasonicModule.WSCallback,
     ...WheelchairModule.WSCallback,
     ...PoseModule.WSCallback,
     ...GoalModule.WSCallback,
--- src/servers/routerRegistry.js
+++ src/servers/routerRegistry.js
@@ -1,14 +1,10 @@
 import { MapModule } from '#modules/map/map.module.js';
-import { ServoModule } from '#modules/servo/servo.module.js';
-import { UltrasonicModule } from '#modules/ultrasonic/ultrasonic.module.js';
 import { WheelchairModule } from '#modules/wheelchair/wheelchair.module.js';
 import { PoseModule } from '#modules/pose/pose.module.js';
 import { GoalModule } from '#modules/goal/goal.module.js';
 
 export const RouterRegistry = [
     ...MapModule.routers,
-    ...ServoModule.routers,
-    ...UltrasonicModule.routers,
     ...WheelchairModule.routers,
     ...PoseModule.routers,
     ...GoalModule.routers,
